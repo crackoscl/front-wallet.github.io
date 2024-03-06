3fb0698b7a17ee54ef6fb4a50c841fc581d8a95b
crackoscl <gilbert.ata@gmail.com>
2024-03-05 23:56:45 -0300

    se crea function alertMensaje para mostrar errores

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
+
+function alertMensaje(mensaje, errorType, fade){
+  $("#alertMensaje").removeClass()
+  $("#alertMensaje").addClass("alert alert-" + errorType)
+  $("#alertMensaje").text("!"+ mensaje)
+  $("#alertMensaje").fadeIn(1000);
+  if(fade){
+    setTimeout(function() { 
+      $('#alertMensaje').fadeOut(1000); 
+  }, 5000);
+  }
+ 
+}
+
+
 $("#login").submit(function (e) {
   e.preventDefault();
   const email = $("#inputEmail").val();
